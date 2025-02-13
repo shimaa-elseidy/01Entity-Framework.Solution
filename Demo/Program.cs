043ebffef868c0e0cde1a4c5f7d44609664cc628
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-02-14 01:58:57 +0200

    02 Ways Of Generation (TPC, TPH, TPCC)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -10,6 +10,12 @@
             // Mapping [Code First : generate table per each class - DB First : generate class per each table]
             // support : L2E [Linq to Entity] : CRUD
             #endregion
+            #region 02 Ways Of Generation (TPC, TPH, TPCC)
+            // TPC  ==> table per class
+            // TPH  ==> table per hierachy
+            // TPCC ==> table per concert class
+            #endregion
         }
     }
 }
+  
