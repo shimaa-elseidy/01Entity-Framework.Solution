eccd9dadac2a379399f24bb65c16508adfb14d8e
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-02-14 01:20:25 +0200

    01. What Is EF Core ?

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region What Is EF Core ?
+            // Entity Framework Core ==> ORM tool into dotnet 
+            // ORM ==> object relation mapper ==> interact between application & database
+            // Mapping [Code First : generate table per each class - DB First : generate class per each table]
+            // support : L2E [Linq to Entity] : CRUD
+            #endregion
         }
     }
 }
