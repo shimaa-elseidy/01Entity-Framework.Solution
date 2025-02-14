8a5f2994b49edb267a268eba9902463156933ecf
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-02-14 12:07:11 +0200

    05 Ways Of Mapping (By Data Annotation)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Demo
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo
 {
     internal class Program
     {
@@ -27,11 +30,37 @@
             //    {
             //        public int Id { get; set; } // Public Numeric property named [ Id | EmployeeId ] --> PK -- identity constraint
             //        public string Name { get; set; } // reference type -- required [ not allow null ] nvarchar(max)
-           //        public int? Age { get; set; } // value type : int [age] - optional [allow null]
-           //        public double Salary { get; set; } // value type : float[salary] - required
-          //        public string? Email { get; set; } // reference type -- nvarchar(max) - optional 
-          //        public DateTime dateTimeCreation { get; set; } // value type - datetime2 - required
-          //    }
+            //        public int? Age { get; set; } // value type : int [age] - optional [allow null]
+            //        public double Salary { get; set; } // value type : float[salary] - required
+            //        public string? Email { get; set; } // reference type -- nvarchar(max) - optional 
+            //        public DateTime dateTimeCreation { get; set; } // value type - datetime2 - required
+            //    }
+            #endregion
+            #region 05 Ways Of Mapping (By Data Annotation)
+            // 2. Data annotation
+            //[Table("Hamada", Schema = "dbo")]
+            //public class Employee
+        //{
+        //    [Key] // Pk has  identity constraint
+        //    [DatabaseGenerated(DatabaseGeneratedOption.None)] // Pk has  identity constraint
+        //    public int EmpId { get; set; }
+        //    [Required]
+        //    [Column("EmpName", TypeName = "varchar")]
+        //    [MaxLength(50)]
+        //    [StringLength(50, MinimumLength = 20)]
+        //    public string Name { get; set; }
+        //    [Range(20, 60)]
+        //    public int? Age { get; set; }
+        //    [EmailAddress]
+        //    public string Email { get; set; }
+        //    [DataType(DataType.PhoneNumber)]
+        //    public string PhoneNumber { get; set; }
+        //    [Column(TypeName = "money")]
+        //    [DataType(DataType.Currency)]
+        //    public double Salary { get; set; }
+        //    [NotMapped]
+        //    public int TotalSalary { get; set; }
+        //}
         #endregion
     }
     }
