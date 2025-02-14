1a768e23d242a2eeaf0493c2ee7b167f4f7d9289
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-02-14 10:51:21 +0200

    04 Ways Of Mapping (By Convension)

---
+++ Demo/Entity/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Entity
+{
+    // EF ==> support 4 ways for mapping classes in data base [table - view - function]
+    // 1. By Convention [ default behavior ]
+    // Entity
+    // POCO class :: Plain OLD CLR Object
+    internal class Employee // table
+    {
+        public int Id { get; set; } // Public Numeric property named [ Id | EmployeeId ] --> PK -- identity constraint
+        public string Name { get; set; } // reference type -- required [ not allow null ] nvarchar(max)
+        public int? Age { get; set; } // value type : int [age] - optional [allow null]
+        public double Salary { get; set; } // value type : float[salary] - required
+        public string? Email { get; set; } // reference type -- nvarchar(max) - optional 
+        public DateTime dateTimeCreation { get; set; } // value type - datetime2 - required
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             #region What Is EF Core ?
             // Entity Framework Core ==> ORM tool into dotnet 
@@ -18,7 +18,22 @@
             #region 03 EF Core Vs Dapper Vs ADO.NET
             // EF Core Vs Dapper Vs ADO.NET
             #endregion
-        }
+            #region 04 Ways Of Mapping (By Convension)
+            // EF ==> support 4 ways for mapping classes in data base [table - view - function]
+            // 1. By Convention [ default behavior ]
+            // Entity
+            // POCO class :: Plain OLD CLR Object
+            //internal class Employee // table
+            //    {
+            //        public int Id { get; set; } // Public Numeric property named [ Id | EmployeeId ] --> PK -- identity constraint
+            //        public string Name { get; set; } // reference type -- required [ not allow null ] nvarchar(max)
+           //        public int? Age { get; set; } // value type : int [age] - optional [allow null]
+           //        public double Salary { get; set; } // value type : float[salary] - required
+          //        public string? Email { get; set; } // reference type -- nvarchar(max) - optional 
+          //        public DateTime dateTimeCreation { get; set; } // value type - datetime2 - required
+          //    }
+        #endregion
+    }
     }
 }
   
